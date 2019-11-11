87ea9601dc4287bd0db7baa92ca10773da4d4659
NicholasJ <geminimumu@gmail.com>
2019-11-12 00:04:39 +0800

    commit final version

--- libra-demo/src/main/resources/sql/libra.sql
+++ libra-demo/src/main/resources/sql/libra.sql
@@ -15,11 +15,7 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-<<<<<<< HEAD
 CREATE DATABASE `libra` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
-=======
-CREATE DATABASE `libra` /*!40100 DEFAULT CHARACTER SET utf8 */;
->>>>>>> branch 'master' of https://github.com/nicholasj425/librademo.git
 
 --
 -- Table structure for table `data_dict`
