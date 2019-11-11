3846597492806f9f25d542eab1d52b56bc8b2ca2
nicholasj425 <50005715+nicholasj425@users.noreply.github.com>
2019-11-11 14:17:29 +0800

    add schema create

--- libra-demo/src/main/resources/sql/libra.sql
+++ libra-demo/src/main/resources/sql/libra.sql
@@ -15,6 +15,8 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+CREATE DATABASE `libra` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
 --
 -- Table structure for table `data_dict`
 --
